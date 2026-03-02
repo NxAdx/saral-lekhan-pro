8f69820d9a4235c3ebcd7f303544dd2b25f9e1bc
“[NxAdx]” <[lokhandeaadarsh28@gmail.com]>
2026-03-02 14:18:21 +0530

    fix: revert sentry-expo (incompatible with SDK 49), use runtime-only @sentry/react-native, restore proven CI patches

--- src/store/notesStore.ts
+++ src/store/notesStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import * as SQLite from 'expo-sqlite';
-import * as Sentry from 'sentry-expo';
+import * as Sentry from '@sentry/react-native';
 import { log } from '../utils/Logger';
 
 // Open (or create) the SQLite database
